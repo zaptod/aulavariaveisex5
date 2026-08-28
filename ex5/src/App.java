6b19078a8fc4814b2ced5f65eb4d61a946108057
ADRIAN OLIVEIRA DE SOUZA <124292306+zaptod@users.noreply.github.com>
2026-08-28 23:49:00 +0000

    ex5

---
+++ ex5/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int largura = 2;
+        int altura = 4;
+        
+        int area = largura * altura; 
+        int perimetro = 2 * (largura + altura); 
+
+        System.out.println("O valor da area é: " + area + "\n e o do perimetro é: " + perimetro);
+    }
+}
